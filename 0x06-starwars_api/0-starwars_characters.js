b1d0cca7df6031a85ffb8af1a49d5e726a0dd5e4
Young-proflat <olatunbosunlateef6@gmail.com>
2024-10-04 02:15:01 +0000

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,23 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
-
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
+const getInfo = (apis, i = 0) => {
+  if (i === apis.length) return;
+  request(apis[i], function (err, res, body) {
+    if (err) {
+      throw err;
     }
-  }));
+    const dta = JSON.parse(body).name;
+    console.log(dta);
+    getInfo(apis, i + 1);
+  });
 };
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error) {
+    throw error;
   }
-};
-
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+  const dta = JSON.parse(body).characters;
+  getInfo(dta);
+  // console.log(dta);
+});
